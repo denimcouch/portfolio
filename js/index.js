597a8f0ccc1c324222216a9963e07ae1ca1b9318
denimcouch <alexmata92@ymail.com>
2020-12-28 12:56:20 -0600

    form test 5.1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(){
-  const contactForm = document.getElementById('form-contact')
-  contactForm.addEventListener('submit', function(e){
-    e.preventDefault()
-    console.log("I've been submitted!")
-    contactForm.reset()
-  })
+  
 })
